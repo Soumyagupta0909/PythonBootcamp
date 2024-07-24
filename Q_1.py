adae7ddb5b7ab7f84dbd121ab3290ed6868ddf04
Soumyagupta0909 <anumulasoumya5@gmail.com>
2024-07-24 11:53:10 +0530

    Day-1

---
+++ Q_1.py
@@ -0,0 +1,10 @@
+#take name and marks from user and print average
+name=input("enter your name")
+print(name)
+sub1=int(input("enter marks"))
+sub2=int(input("enter marks"))
+sub3=int(input("enter marks"))
+sub4=int(input("enter marks"))
+sum=sub1+sub2+sub3+sub4
+avg=sum/4
+print(f"hello {name} your marks {avg}")
