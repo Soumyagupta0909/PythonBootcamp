cbe21dc2c808109d83d730f00455e3e046d1eb4e
Soumyagupta0909 <anumulasoumya5@gmail.com>
2024-07-24 15:42:36 +0530

    Day-2

---
+++ hw2.py
@@ -0,0 +1,29 @@
+#write a program to find area of circle
+# wap to find perimeter of a circle
+#wap to find area of a triangle
+#wap to find perimeter of a traingle
+
+print("AREA OF CIRCLE")
+r=int(input("enter radius"))
+area=3.14*r*r
+print(area)
+
+
+print("PERIMETER OF CIRCLE")
+r=int(input("enter radius"))
+perimeter=2*3.14*r
+print(perimeter)
+
+
+print("AREA OF TRIANGLE")
+b=int(input("enter base of triangle"))
+h=int(input("enter height of triangle"))
+area=0.5*b*h
+print(area)
+
+print("perimeter OF TRIANGLE")
+s1=int(input("enter side1"))
+s2=int(input("enter side2"))
+s3=int(input("enter side3"))
+perimeter=s1+s2+s3
+print(perimeter)
